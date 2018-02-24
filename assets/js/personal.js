04d0e7c5ac16de63a0138dd3e4adc0e9695ce7d1
Fred Zhang <zhangxu.fred@gmail.com>
2018-02-24 20:59:25 +0800

    Update

---
+++ assets/js/personal.js
@@ -0,0 +1,9 @@
+function playAudio () {
+    let audio = document.getElementById('hold-your-hand');
+    audio.play();
+}
+
+function pauseAudio () {
+    let audio = document.getElementById('hold-your-hand')
+    audio.pause();
+}
